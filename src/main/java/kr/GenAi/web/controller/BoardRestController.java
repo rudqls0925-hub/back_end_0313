19b5c55d4d33ab9da6f2363bd310399d6abab098
rudqls0925 <rudqls09250@gmail.com>
2026-03-13 12:37:04 +0900

    update

--- src/main/java/kr/GenAi/web/controller/BoardRestController.java
+++ src/main/java/kr/GenAi/web/controller/BoardRestController.java
@@ -17,7 +17,8 @@ import kr.GenAi.web.service.BoardService;
 
 @RestController
 @RequestMapping("/api/board")
-@CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
+// @CrossOrigin(origins = "http://127.0.0.1:5500", allowedHeaders = "*")
+@CrossOrigin(origins = "http://10.1.1.6", allowedHeaders = "*")
 public class BoardRestController {
 	
 	@Autowired
